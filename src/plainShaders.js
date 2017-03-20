dcbd71bb1d36a996a8aac07b83b6728604a632f7
Duncan Walter <dwalter@barlingarts.com>
2017-03-19 20:39:49 -0500

    added the enigmagon to this folder, etc

--- src/plainShaders.js
+++ src/plainShaders.js
@@ -1,7 +1,6 @@
-
-// in order to a) appease twgl, b) work within security standards, and c) keep a slim html file,
-// shaders are hard-loaded into the DOM here.
-
+/**
+ * Created by Duncan on 3/8/2017.
+ */
 define(function(){
 
     var vertexId = "plain-vertex"; // vertexId: (ScriptId :: String)
@@ -10,30 +9,34 @@ define(function(){
     var head = $("head");
 
     head.append("<script id='"+vertexId+"' type='sglsl'>\
+        \
         uniform mat4 u_projection;\
         \
         attribute vec3 a_position;\
-        attribute vec3 a_color;\
+        attribute vec4 a_color;\
         \
-        \
-        \
-        varying vec3 v_color;\
+        varying vec4 v_color;\
         \
         void main() {\
+        \
             v_color = a_color;\
             gl_Position = u_projection * vec4(a_position, 1.0);\
+        \
         }\
+        \
         </script>"
     );
 
     head.append("<script id='"+fragmentId+"' type='sglsl'>\
         precision mediump float;\
         \
-        varying vec3 v_color;\
+        varying vec4 v_color;\
         \
         void main(){\
-            gl_FragColor = vec4(v_color, 1.0);\
+        \
+            gl_FragColor = v_color;\
         }\
+        \
         </script>"
     );
 
